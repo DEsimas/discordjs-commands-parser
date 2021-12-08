d01b9a32729568adcf0568e326ae2d9f285e404d
DEsimas <simasuh@gmail.com>
2021-12-08 21:33:16 +0300

    working prototype

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,28 @@
-import { hello } from "./hello";
+import { CommandsHandler, ICommand, ICommandHandler, ICommandsHandler, IPayload } from "./DiscordCommandsParser";
 
 import * as Discord from "discord.js";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(hello(process.env.TOKEN ? process.env.TOKEN : "undefined"));
+class gayWebsite implements ICommandHandler {
+    execute(payload: IPayload) {
+        console.log(payload);
+    }
+}
 
-/*try {
-    dotenv.config();
-    const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
-    
-    client.login(process.env.TOKEN);
-} catch(err) {
-    console.log(err);
-}*/
+const list: Array<ICommand> = [
+    {
+        name: ["OwO"],
+        out: new gayWebsite,
+        multicase: true
+    }
+];
+
+const handler: ICommandsHandler = new CommandsHandler(list, "|");
+
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on("messageCreate", handler.getEventHandler());
+
+client.login(process.env.TOKEN);
