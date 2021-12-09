246b4dac7236f421b21fbab8c9310f12586e47d2
DEsimas <simasuh@gmail.com>
2021-12-09 23:01:43 +0300

    package created

--- src/CommandsParser.ts
+++ src/CommandsParser.ts
@@ -1,20 +1,18 @@
 import { Message } from 'discord.js';
-import { ICommand, ICommandsHandler, IMiddleware, INext, IParserOptions, IPayload } from './types';
+import { ICommandHandler, ICommand, ICommandsHandler, IMiddleware, INext, IParserOptions, IPayload } from './types';
 
-export { ICommand, ICommandsHandler, IMiddleware, INext, IParserOptions, IPayload };
-
-export class CommandsHandler implements ICommandsHandler {
+class CommandsHandler implements ICommandsHandler {
 	private commands: Array<ICommand> = [];
 	private prefix: string;
 	private middlewares: Array<IMiddleware>;
-
+	
 	constructor(options: IParserOptions) {
 		this.commands = options.commandsList;
 		this.prefix = options.prefix;
 		this.middlewares = options.middlewares ? options.middlewares : [];
 		this.middlewares.push(this.commandHandler);
 	}
-
+	
 	private commandHandler(payload: IPayload): void {
 		payload.commands.every(command => {
 			return command.name.every(name => {
@@ -26,18 +24,18 @@ export class CommandsHandler implements ICommandsHandler {
 			});
 		});
 	}
-
+	
 	private getNextMiddleware(middlewares: Array<IMiddleware>, counter: number): INext {
 		return (payload: IPayload) => {
 			middlewares[counter](payload, this.getNextMiddleware(middlewares, ++counter));
 		}
 	}
-
+	
 	public getEventHandler() {
 		return (message: Message): void => {
 			const content:string = message.content.trim();
 			const args:Array<string> = content.split(" ");
-
+			
 			const payload: IPayload = {
 				prefix: this.prefix,
 				message: message,
@@ -50,3 +48,5 @@ export class CommandsHandler implements ICommandsHandler {
 		};
 	}
 };
+
+export { CommandsHandler, ICommandHandler, ICommand, ICommandsHandler, IMiddleware, INext, IParserOptions, IPayload }
