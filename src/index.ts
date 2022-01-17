3841f5f4d84b34fad98f761f9d61e500f64c3103
DEsimas <simasuh@gmail.com>
2022-01-17 16:45:59 +0300

    export moved to index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { CommandsParser } from "./CommandsParser"
+import { CommandHandler, Payload, Command, Middleware, Next, ParserOptions } from "./types"
+
+export { CommandsParser, CommandHandler, Payload, Command, Middleware, Next, ParserOptions };
